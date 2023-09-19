5c219a040e7ce074594e679d287b0932ae557aea
Amelia Tang <amelia01272007@gmail.com>
2023-09-19 10:53:57 -0700

    Added some new notes and python files

---
+++ 1 - Natural Language Processing.md
@@ -0,0 +1,29 @@
+
+Properties
+
+#notes #programming 
+
+
+
+
+### Output
+We can display text and symbols using output
+
+We us the print( ) function to. display output
+
+
+
+```python
+print ( )
+```
+
+
+
+
+
+
+# Comments
+Comments are pieces of text that are Not interpreted by Python 
+This means that the text is ignored
+We us the # symbol to make comments
+
---
+++ quotegenerator.py
@@ -0,0 +1,15 @@
+# Quote Generator
+# Author: Ubial
+# Date: 19 September 2023
+
+# Output a quote from someone famous
+print ("Do or do not. There is no try. --Yoda")
+
+# Output my favourite food
+print ("apples --Amelia")
+
+# Ask the user a question
+input("What was your favourite quote?")
+
+# Respond with something a human would say
+print("Oh, that's my favourit too!")
