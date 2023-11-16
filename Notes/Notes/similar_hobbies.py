48e89e89d3d61be63a45c52f3e16b1e098078b6c
Amelia Tang <amelia01272007@gmail.com>
2023-11-16 10:58:29 -0800

    hobby finished

---
+++ Notes/Notes/similar_hobbies.py
@@ -0,0 +1,19 @@
+# Hobbies similarity score
+# Author: Ruby
+# Date: 14th Nov 2023
+
+print("User 1, please enter hobbies separated by spaces:")
+user1_hobbies = input().lower().split()
+
+print("User 2, please enter hobbies separated by spaces:")
+user2_hobbies = input().lower().split()
+
+# Calculate similarity score based on the number of common hobbies
+similarity_score = 0
+
+for hobby in user1_hobbies:
+    if hobby in user2_hobbies:
+        similarity_score += 1
+
+# Display the result
+print(f"The similarity score between the users is: {similarity_score}")
--- Notes/sim_score_sfu_best.py
+++ Notes/sim_score_sfu_best.py
@@ -1,47 +1,60 @@
-# SFU bEST - Similarity score
-# Author: Amelia Tang
-# Date November 9 2023
-
-
+# SFUs Best - Similarity Score
+# Author: Ubial
+# 10 November 2023
 
 # Load data from a file
-# "Read" it in a meanful
+# "Read" it in a meaningful
 # Link our Sim Score algo to the data
 
 # Open the file
 with open("./data.csv") as f:
-   f.readline()
-   
-f.readline ()
+    f.readline()
+
+    # The second line of the csv file
+    f.readline()
+
+# Create a "profile" of likes (fave places
+# in SFU)
+profile = [
+    "Bubble World",
+    "Chef Hung",
+    "Uncle Fatih's",
+    "Guadalupe (MBC)",
+    "Steve's Poke Bar"
+]
+
+# Initialize top score and their name
+most_similar_score = 0
+most_similar_name = ""
+
+with open("./data.csv") as f:
+    # Throw away the header
+    header = f.readline()
+
+    for line in f:
+        # Convert the string to a list
+        current_likes = line.split(",")
+
+        # Store the person's name
+        current_name = current_likes[1]
+        
+        # Initialize the current sim score
+        current_sim_score = 0
+
+        # Sim score algorithm
+        for item in profile:
+            if item in current_likes:
+                current_sim_score += 1
+        
+        # Print the results from this line of data
+        print(f"{current_name} - Score: {current_sim_score}")
+
+        # Update the most similar person
+        if current_sim_score > most_similar_score:
+            most_similar_score = current_sim_score
+            most_similar_name = current_name
+
+print("🏅🏅🏅🏅 Most similar person! 🏅🏅🏅🏅")
+print(f"{most_similar_name} - Score: {most_similar_score}")
 
 
-#Create a "profile" of likes
-# (fave places in SFU)
-profile = 
-"Bubble World",
-"Chef Huang",
-"Guadalupe (MBC)",
-"Steves's poke Bar"
-
-with open ("./data.csv")as f:
-   # Throw away the header
-   header = f.readline()
-
-   for line in f:
-      #convert the string to a list
-      curent_likes = line.split(",")
-
-   #store the person's name
-current_name = current_likes[1]
-
-
-   # Initalize the current sim score
-currrent_sim_score = 0
-
-# Sim score algorithm
-for item in profile:
-   if item in cureent_likes:
-      current_sim_score += 1
-
-# Print the results from this line of data
-print(f"{current_name }")
