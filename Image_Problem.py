3fd11a401f5e511c7073da0c6aae64f9bb127932
Amelia Tang <amelia01272007@gmail.com>
2024-01-11 10:54:58 -0800

    done

--- Image_Problem.py
+++ Image_Problem.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 import colour_helper
 
-# variable for colours
+# Test?
 light_pixel = (255, 255, 255)
 light_gray = (128, 128, 128)
 darker_gray = (100, 128, 128)
@@ -20,22 +20,67 @@ print(colour_helper.is_light(reddish_pixel))  # False
 print(colour_helper.is_light(dark_gray))  # False
 print(colour_helper.is_light(dark_pixel))  # False
 
-# Open the image
-with Image.open("./Images/binary-image.png") as im:
-    image_height = im.height
-    image_width = im.width
 
-    # starting at the top and working our way down
-    # visit the pixels from left to right
-    for y in range(image_height):
-        for x in range(image_width):
-            pixel = im.getpixel((x, y))
+def binarize() -> None:
+    """Binarizes an image"""
 
-            # if the pixel is light
-            if colour_helper.is_light(pixel):
-                im.putpixel((x, y), light_pixel)
-            else:
-                im.putpixel((x, y), dark_pixel)
+    # Open the image
+    with Image.open("./Images/catto.png") as im:
+        image_height = im.height
+        image_width = im.width
 
-    # After visiting every pixel, save the image
-    im.save("./Images/binarized.jpg")
+        # starting at the top and working our way down
+        # visit the pixels from left to right
+        for y in range(image_height):
+            for x in range(image_width):
+                pixel = im.getpixel((x, y))
+
+                # if the pixel is light
+                if colour_helper.is_light(pixel):
+                    im.putpixel((x, y), light_pixel)
+                else:
+                    im.putpixel((x, y), dark_pixel)
+
+        # After visiting every pixel, save the image
+        im.save("./Images/binarized.jpg")
+
+
+def picture_to_grayscale(filename: str) -> None:
+    """Convert a given picture to grayscale"""
+
+    # Open up the image
+    with Image.open(f"./Images/{filename}") as im:
+        # Visit every pixel
+        for y in range(im.height):
+            for x in range(im.width):
+                pixel = im.getpixel((x, y))
+
+                # Take that pixel and convert it to gray
+                gray_pixel = colour_helper.pixel_to_grayscale(pixel)
+
+                im.putpixel((x, y), gray_pixel)
+
+        # Save the image
+        im.save("./Images/grayscale.jpg")
+
+
+def random_picture_effect(filename: str) -> None:
+    """Convert a given picture to something random"""
+
+    # Open up the image
+    with Image.open(f"./Images/{filename}") as im:
+        # Visit every pixel
+        for y in range(im.height):
+            for x in range(im.width):
+                pixel = im.getpixel((x, y))
+
+                # Take that pixel and randomize it
+                random_pixel = colour_helper.pixel_to_random_effect(pixel)
+                im.putpixel((x, y), random_pixel)
+
+        # Save the image
+        im.save("./Images/random.jpg")
+
+
+# picture_to_grayscale("best_pizza.jpg")
+random_picture_effect("best_pizza.jpg")
--- colour_helper.py
+++ colour_helper.py
@@ -2,6 +2,31 @@
 # Functions that help wither colours
 
 
+def pixel_to_name(pixel: tuple) -> str:
+    """Given a 3-tuple, return a string representing
+    its colour
+
+    Params:
+        pixel = 3-tuple of values (red, green, blue)
+
+    Returns:
+        name of the colour
+    """
+    red, green, blue = pixel
+
+    # TODO: detect red pixels
+    if red < 200 and blue < 200 and green > 220:
+        return "green"
+    elif red > 170 and green < 60 and blue < 60:
+        return "red"
+    else:
+        return "colour unknown"
+
+
+print(pixel_to_name((180, 3, 2)))
+print(pixel_to_name((255, 255, 255)))
+
+
 def is_light(pixel: tuple) -> bool:
     """Returns True if the pixel is a "light" pixel
 
@@ -21,4 +46,31 @@ def is_light(pixel: tuple) -> bool:
     if average >= 128:
         return True
     else:
-     return False
+        return False
+
+
+def pixel_to_grayscale(pixel: tuple) -> tuple:
+    """Return a gray version of the given pixel"""
+    red, green, blue = pixel
+
+    gray = int(red * 0.3 + green * 0.59 + blue * 0.11)
+
+    return (gray, gray, gray)
+
+
+def pixel_to_random_effect(pixel: tuple) -> tuple:
+    """Return a random pixel"""
+    red, green, blue = pixel
+
+    red += 30
+    green += 50
+    blue -= 10
+
+    if red > 255:
+        red = 255
+    if green > 255:
+        green = 255
+    if blue < 0:
+        blue = 0
+
+    return (red, green, blue)
